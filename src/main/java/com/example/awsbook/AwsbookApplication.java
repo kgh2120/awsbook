239df20dd0db49a60bd16a6b4eddcfd1653d8eb7
kyoohyun <kgh2120@naver.com>
2022-06-20 12:48:51 +0900

    Initial commit

---
+++ src/main/java/com/example/awsbook/AwsbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsbook/AwsbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
