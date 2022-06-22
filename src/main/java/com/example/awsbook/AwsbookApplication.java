447db7bc27a9712cf64caa9f5cea9bf3a61e27a1
kyoohyun <kgh2120@naver.com>
2022-06-22 18:19:33 +0900

    test

--- src/main/java/com/example/awsbook/AwsbookApplication.java
+++ src/main/java/com/example/awsbook/AwsbookApplication.java
@@ -1,6 +1,8 @@
 package com.example.awsbook;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -9,7 +11,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AwsbookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AwsbookApplication.class, args);
+
+
+        SpringApplication application = new SpringApplication(AwsbookApplication.class);
+        application.setWebApplicationType(WebApplicationType.SERVLET);
+        application.setBannerMode(Banner.Mode.OFF);     // 배너 감추기
+        application.run(args);
     }
 
 }
