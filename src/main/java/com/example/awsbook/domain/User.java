e350b99def755d3a0c263b0457b4c31898bcac10
kyoohyun <kgh2120@naver.com>
2022-06-27 20:16:20 +0900

    User entity의 pk생성전략 수정

--- src/main/java/com/example/awsbook/domain/User.java
+++ src/main/java/com/example/awsbook/domain/User.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity @NoArgsConstructor @Getter
 public class User extends BaseEntity{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
