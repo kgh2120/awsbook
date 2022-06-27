f5a08fa28c6a52841ef841a81b0636a3a01189f9
kyoohyun <kgh2120@naver.com>
2022-06-27 16:48:41 +0900

    ExceptionHandler 생성

---
+++ src/main/java/com/example/awsbook/exception/ExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.example.awsbook.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse {
+
+    private LocalDateTime timeStamp;
+    private String message;
+    private String detail;
+}
