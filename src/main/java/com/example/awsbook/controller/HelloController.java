081ee016b8c90830a5b7d725fdbe60bd3804a146
kyoohyun <kgh2120@naver.com>
2022-06-27 17:42:08 +0900

    타임리프 테스트

--- src/main/java/com/example/awsbook/controller/HelloController.java
+++ src/main/java/com/example/awsbook/controller/HelloController.java
@@ -1,26 +1,36 @@
 package com.example.awsbook.controller;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RequestMapping("hello")
-@RestController
+@Controller
 public class HelloController {
 
 
+    @ResponseBody
     @GetMapping("/korea")
     public ResponseEntity<String> helloKorea() {
         String korea = "hello~ korea";
         return ResponseEntity.ok(korea);
     }
-
+    @ResponseBody
     @GetMapping("/japan")
     public ResponseEntity<String> helloJapan() {
         String japan = "hello~ japan";
         return ResponseEntity.ok(japan);
     }
+
+    @GetMapping("/leaf")
+    public String helloLeaf() {
+        return "helloLeaf";
+    }
+
+
 }
